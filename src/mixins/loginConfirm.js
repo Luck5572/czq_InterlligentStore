067b4037eab69ad19b89349e8545d038eee0a6aa
czq5572 <shangxuanj6@gmail.com>
2025-08-22 17:25:25 +0800

    初始提交：智慧商城项目

---
+++ src/mixins/loginConfirm.js
@@ -0,0 +1,34 @@
+export default {
+  created () {
+  },
+  data () {
+    return {
+      title: '标题'
+    }
+  },
+  methods: {
+    loginConfirm () {
+      // 判断 token 是否存在
+      if (!this.$store.getters.token) {
+        // 弹确认框
+        this.$dialog.confirm({
+          title: '温馨提示',
+          message: '此时需要先登录才能继续操作哦',
+          confirmButtonText: '去登陆',
+          cancelButtonText: '再逛逛'
+        })
+          .then(() => {
+            this.$router.replace({
+              path: '/login',
+              query: {
+                backUrl: this.$route.fullPath
+              }
+            })
+          })
+          .catch(() => {})
+        return true
+      }
+      return false
+    }
+  }
+}
---
+++ src/utils/request.js
@@ -0,0 +1,49 @@
+import store from '@/store'
+import axios from 'axios'
+import { Toast } from 'vant'
+
+const instance = axios.create({
+  baseURL: 'http://smart-shop.itheima.net/index.php?s=/api',
+  timeout: 4000,
+  Header: { platform: 'h5' }
+
+})
+
+// 添加请求拦截器
+instance.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  Toast.loading({
+    message: '加载中...',
+    forbidClick: true,
+    loadingType: 'spinner',
+    duration: 0
+  })
+
+  const token = store.getters.token
+  if (token) {
+    config.headers['Access-Token'] = token
+    config.headers.platform = 'H5'
+  }
+
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error)
+})
+
+instance.interceptors.response.use(function (response) {
+  const res = response.data
+  if (res.status !== 200) {
+    Toast(res.message)
+
+    return Promise.reject(res.message)
+  } else {
+    Toast.clear()
+  }
+  return res
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error)
+})
+
+export default instance
